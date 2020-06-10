3b982eaccd66cb73e474dd15b251d7aaf5e48b08
Jane <jane.e.nath@gmail.com>
2020-06-10 10:45:19 -0700

    create and style basic page layout

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ function App() {
         <button className="instructions">instructions</button>
       </header>
       <body>
-        <img className="plantImage" src="https://i.imgur.com/oj3KRB2.png"></img>
+        <div className="plantContainer">
+          <img className="plantImage" src="https://i.imgur.com/oj3KRB2.png"></img>
+          <div className="actionsContainer">
+            <button className="actionButton"><img className="actionImage" src="https://i.imgur.com/Cd69PTX.png" alt="watering can"></img>water plant</button>
+            <button className="actionButton"><img className="actionImage" src="https://i.imgur.com/agzi7zf.png" alt="fertilizer"></img>feed plant</button>
+            <button className="actionButton"><img className="actionImage" src="https://i.imgur.com/xfU5t06.png" alt="sun"></img>move plant to sun</button>
+          </div>
+        </div>
       </body>
       <footer>
         <p>©2020 <a href="https://www.janenath.com" target="_blank" rel="noopener noreferrer">Jane Nath</a></p>
